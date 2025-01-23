aef8fae54715bf0c6bc568e72acca6a5a24c7b5d
OLGAPOPKOVA210470 <popkovaolga1970@gmail.com>
2025-01-23 22:08:21 +0300

    проверка связи

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+const navToggle = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav');
+
+nav.classList.add('nav--closed');
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--closed');
+    nav.classList.toggle('nav--opened');
+})
