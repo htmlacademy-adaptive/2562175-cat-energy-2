54c7e5daa82fc8ca3339fa1e2a96e9eca9d9d994
OLGAPOPKOVA210470 <popkovaolga1970@gmail.com>
2025-01-25 21:14:28 +0300

    исправляет код

---
+++ source/source/scripts/index.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav');
+
+nav.classList.add('nav--closed');
+
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('nav--closed');
+  nav.classList.toggle('nav--opened');
+});
