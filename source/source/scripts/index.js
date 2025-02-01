e87943a99a1f5dc1e61db4361685a18fedb5b894
OLGAPOPKOVA210470 <popkovaolga1970@gmail.com>
2025-02-01 23:05:02 +0300

    добавляет карту

--- source/source/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const navToggle = document.querySelector('.nav__toggle');
-const nav = document.querySelector('.nav');
-
-nav.classList.add('nav--closed');
-
-navToggle.addEventListener('click', () => {
-  nav.classList.toggle('nav--closed');
-  nav.classList.toggle('nav--opened');
-});
